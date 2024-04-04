b41d94e0e13664ba74aa0d8cfddb9591ca705a96
yohuang <717146638@qq.com>
2024-04-04 15:12:17 +0800

    新建各个模块，并接入springboot，测试成功

---
+++ austin-web/src/main/java/com/javayh/austin/AustinApplication.java
@@ -0,0 +1,11 @@
+package com.javayh.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class,args);
+    }
+}
