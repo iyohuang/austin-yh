a6a5937ec1136d99267133dd810f6111c25c77f5
yohuang <717146638@qq.com>
2024-04-04 16:51:56 +0800

    引入日志框架logback

--- austin-web/src/main/java/com/javayh/austin/web/TestController.java
+++ austin-web/src/main/java/com/javayh/austin/web/TestController.java
@@ -1,12 +1,15 @@
 package com.javayh.austin.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     private String test(){
+        log.info("----------------------日志启动成功------------------");
         return "hello,yh";
     }
 }
